d740fe18c68c2288700f2c86ccb0a99bfa8e9700
alikahwaji <alikahwaji.kahwaji@hotmail.com>
2017-08-13 14:34:52 +1200

    Creating logic operator

---
+++ logic-operators.R
@@ -0,0 +1,34 @@
+#logical:
+#TRUE
+#FALSE
+
+4 < 5
+10 > 100
+
+4 == 5
+4 == 4
+# ==
+#!=
+
+# >
+#>=
+#<
+#<=
+#!
+#|
+#&
+#isTRUE(x)
+
+result <-  4 < 5 
+typeof(result)
+
+result2 <- !TRUE
+result3 <- !(5>1)
+
+result | result2
+result2 | result3
+
+result & result2
+
+isTRUE(result)
+isTRUE(result2)
